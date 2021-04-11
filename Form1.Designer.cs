b481b28e4c4c5bc9501d58460b0f97ccdb263136
LordAids <62069378+LordAids@users.noreply.github.com>
2021-04-11 18:30:31 +0300

    StartWork

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,57 @@ namespace OfflineChat
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.chatWindow = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chatWindow
+            // 
+            this.chatWindow.Location = new System.Drawing.Point(12, 12);
+            this.chatWindow.Name = "chatWindow";
+            this.chatWindow.Size = new System.Drawing.Size(699, 330);
+            this.chatWindow.TabIndex = 0;
+            this.chatWindow.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 365);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(558, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(576, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(724, 401);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chatWindow);
+            this.Name = "Form1";
+            this.Text = "Оффлайн чат";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox chatWindow;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using StackExchange.Redis;
 
 namespace OfflineChat
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var option = new ConfigurationOptions();
+            option.EndPoints.Add("localhost", 6379);
+
+            var redis = ConnectionMultiplexer.Connect(option);
+            var db = redis.GetDatabase(1);
+
+            const string Key = "Chat1";
+            db.StringSet(Key, "Чат начался!");
+
+            chatWindow.Text = db.StringGet(Key);
+
+            /*db.StringSet(Key, "Znachenie");
+
+            var result = db.StringGet(Key);*/
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
